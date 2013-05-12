4bf75fb3c950a201ee95c7c6fa4e2a76397b7809
Okuno Zankoku <zankoku.okuno@gmail.com>
2013-05-12 13:43:59 -0400

    refactor to reduce complexity

--- src/main.d
+++ src/main.d
@@ -1,5 +1,8 @@
+
 import dx.string : chomp, contains_any;
 import std.string : strip;
+
+import dx.testing;
 
 
 final class SyntaxError : Exception {
@@ -7,136 +10,156 @@ final class SyntaxError : Exception {
         super(msg, file, line, next);
     }
 }
+    enum State { START, SCALAR, ARRAY, END }
+    enum Sigil { DATA, SCALAR, ARRAY, NEXT }
 
 class Environment {
     this(in string[] lines) {
         Parser p = new Parser();
         foreach(string line; lines)
-            p.parse(Line(line));
+            p.parse(line);
         p.flush();
     }
     //TESTME
 
     private:
-    enum State { START, SCALAR, ARRAY, END }
-    enum Sigil { DATA, SCALAR, ARRAY, NEXT }
 
     //SPIFFY and this why I like D
     string[string] scalars;
     string[][string] vectors;
 
-    class Parser {
-        State state = State.START;
-        private string _current_key = null;
-        @property string current_key() { return this._current_key; }
-        @property string current_key(string value)
-        in {
-            assert(value !is null);
-            assert(!value.contains_any("\n\r"));
-        }
-        body {
-            value = value.strip();
-            if (value.length == 0) throw new Exception("Zero-length identifiers not allowed.");
-            return this._current_key = value;
-        }//TESTME
-        string scalar_data = "";
-        string[] array_data = [];
 
-        void parse(in Line line) {
-            switch (this.state) {
-                case State.START: {
-                    switch (line.sigil) {
-                        case Sigil.DATA: throw new SyntaxError("Data encountered before key established.");
-                        case Sigil.NEXT: throw new SyntaxError("Array delimiter encountered before array established.");
-                        case Sigil.SCALAR:
-                            this.current_key = line.data;
-                            this.state = State.SCALAR;
-                            break;
-                        case Sigil.ARRAY:
-                            this.current_key = line.data;
-                            this.state = State.ARRAY;
-                            break;
-                        default: assert(false);
-                    }
-                    break;
+    
+}//Environment
+
+
+
+private final
+class Parser {
+    this(Environment env=null) { this.outer = env; }
+private:
+    Environment outer;
+    State state = State.START;
+    private string _current_key = null;
+    string scalar_data = "";
+    string[] array_data = [];
+    
+public:
+    @property string current_key() { return this._current_key; }
+    @property string current_key(string value)
+    in {
+        assert(value !is null);
+        assert(!value.contains_any("\n\r"));
+    }
+    body {
+        value = value.strip();
+        if (value.length == 0) throw new Exception("Zero-length identifiers not allowed.");
+        return this._current_key = value;
+    }
+    unittest { run_test({
+        Parser p = new Parser();
+        p.current_key = "okay";
+        assert(does_throw!Exception(p.current_key = ""));
+    });}
+        
+
+    void parse(in string raw) {
+        auto line = Line(raw);
+        switch (this.state) {
+            case State.START: {
+                switch (line.sigil) {
+                    case Sigil.DATA: throw new SyntaxError("Data encountered before key established.");
+                    case Sigil.NEXT: throw new SyntaxError("Array delimiter encountered before array established.");
+                    case Sigil.SCALAR:
+                        this.current_key = line.data;
+                        this.state = State.SCALAR;
+                        break;
+                    case Sigil.ARRAY:
+                        this.current_key = line.data;
+                        this.state = State.ARRAY;
+                        break;
+                    default: assert(false);
                 }
-                case State.SCALAR: {
-                    switch (line.sigil) {
-                        case Sigil.DATA: buffer_data(line.data); break;
-                        case Sigil.NEXT: throw new SyntaxError("Array delimiter encountered in scalar.");
-                        case Sigil.SCALAR:
-                            this.flush(State.SCALAR);
-                            this.current_key = line.data;
-                            break;
-                        case Sigil.ARRAY:
-                            this.flush(State.ARRAY);
-                            this.current_key = line.data;
-                            break;
-                        default: assert(false);
-                    }
-                    break;
-                }
-                case State.ARRAY: {
-                    switch (line.sigil) {
-                        case Sigil.DATA: buffer_data(line.data); break;
-                        case Sigil.NEXT: this.buffer_elem(); break;
-                        case Sigil.SCALAR:
-                            this.flush(State.SCALAR);
-                            this.current_key = line.data;
-                            break;
-                        case Sigil.ARRAY:
-                            this.flush(State.ARRAY);
-                            this.current_key = line.data;
-                            break;
-                        default: assert(false);
-                    }
-                    break;
-                }
-                case State.END:
-                    //TODO better exception type
-                    throw new Exception("Further parsing attempted after end of input reached.");
-                default: assert(false);
+                break;
             }
-        }//Parser.parse
-        //TESTME
-
-        void buffer_data(string data) {
-            if (this.scalar_data != "") this.scalar_data ~= '\n';
-            this.scalar_data ~= data;
+            case State.SCALAR: {
+                switch (line.sigil) {
+                    case Sigil.DATA: buffer_data(line.data); break;
+                    case Sigil.NEXT: throw new SyntaxError("Array delimiter encountered in scalar.");
+                    case Sigil.SCALAR:
+                        this.flush(State.SCALAR);
+                        this.current_key = line.data;
+                        break;
+                    case Sigil.ARRAY:
+                        this.flush(State.ARRAY);
+                        this.current_key = line.data;
+                        break;
+                    default: assert(false);
+                }
+                break;
+            }
+            case State.ARRAY: {
+                switch (line.sigil) {
+                    case Sigil.DATA: buffer_data(line.data); break;
+                    case Sigil.NEXT: this.buffer_elem(); break;
+                    case Sigil.SCALAR:
+                        this.flush(State.SCALAR);
+                        this.current_key = line.data;
+                        break;
+                    case Sigil.ARRAY:
+                        this.flush(State.ARRAY);
+                        this.current_key = line.data;
+                        break;
+                    default: assert(false);
+                }
+                break;
+            }
+            case State.END:
+                //TODO better exception type
+                throw new Exception("Further parsing attempted after end of input reached.");
+            default: assert(false);
         }
-        void buffer_elem() {
-            this.array_data ~= this.scalar_data;
+    }//Parser.parse
+    //TESTME
+
+    void flush(State new_state=State.END)
+    in {
+        assert(this.state != State.START);
+        assert(this.current_key !is null);
+    }
+    out {
+        assert(this.scalar_data == "");
+        assert(this.array_data == []);
+        assert(this.current_key is null);
+    }
+    body {
+        if (this.state == State.SCALAR) {
+            this.outer.scalars[current_key] = this.scalar_data;
             this.scalar_data = "";
         }
+        else {
+            assert(this.state == State.ARRAY);
+            this.buffer_elem();
+            this.outer.vectors[current_key] = this.array_data;
+            this.array_data = [];
+        }
+        this._current_key = null;
+        this.state = new_state;
+    }
+    //TESTME
 
-        void flush(State new_state=State.END)
-        in {
-            assert(this.state != State.START);
-            assert(this.current_key !is null);
-        }
-        out {
-            assert(this.scalar_data == "");
-            assert(this.array_data == []);
-            assert(this.current_key is null);
-        }
-        body {
-            if (this.state == State.SCALAR) {
-                this.outer.scalars[current_key] = this.scalar_data;
-                this.scalar_data = "";
-            }
-            else {
-                assert(this.state == State.ARRAY);
-                this.buffer_elem();
-                this.outer.vectors[current_key] = this.array_data;
-                this.array_data = [];
-            }
-            this._current_key = null;
-            this.state = new_state;
-        }
-        //TESTME
-    }//Parser
-    
-    static struct Line {
+private:
+    void buffer_data(string data) {
+        if (this.scalar_data != "") this.scalar_data ~= '\n';
+        this.scalar_data ~= data;
+    }
+    void buffer_elem() {
+        this.array_data ~= this.scalar_data;
+        this.scalar_data = "";
+    }
+
+    static final
+    struct Line {
     public:
         Sigil sigil;
         string data;
@@ -146,7 +169,7 @@ class Environment {
         }
         //TESTME
 
-        private:
+    private:
         static
         Sigil sigil_detect(in string raw) {
             if (raw.length < 1) throw new SyntaxError("Each line must begin with a sigil.");
@@ -160,8 +183,5 @@ class Environment {
         }
         //TESTME
     }//Line
-}//Environment
 
-
-
-
+    }//Parser
