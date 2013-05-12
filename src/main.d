1419fd3039c2ea7a91f53d4ae2f7ef937b01f9bd
Okuno Zankoku <zankoku.okuno@gmail.com>
2013-05-12 14:26:00 -0400

    added some unittests, fixed one bug

--- src/main.d
+++ src/main.d
@@ -15,7 +15,7 @@ final class SyntaxError : Exception {
 
 class Environment {
     this(in string[] lines) {
-        Parser p = new Parser();
+        Parser p = new Parser(this);
         foreach(string line; lines)
             p.parse(line);
         p.flush();
@@ -56,12 +56,11 @@ public:
         if (value.length == 0) throw new Exception("Zero-length identifiers not allowed.");
         return this._current_key = value;
     }
-    unittest { run_test({
-        Parser p = new Parser();
-        p.current_key = "okay";
-        assert(does_throw!Exception(p.current_key = ""));
-    });}
-        
+    unittest { 
+        auto p = new Parser();
+        run_test({ p.current_key = "okay"; });
+        run_test({ assert(does_throw!Exception(p.current_key = "")); });
+    }
 
     void parse(in string raw) {
         auto line = Line(raw);
@@ -100,8 +99,8 @@ public:
             }
             case State.ARRAY: {
                 switch (line.sigil) {
-                    case Sigil.DATA: buffer_data(line.data); break;
-                    case Sigil.NEXT: this.buffer_elem(); break;
+                    case Sigil.NEXT: this.buffer_elem(); //fallthrough
+                    case Sigil.DATA: this.buffer_data(line.data); break;
                     case Sigil.SCALAR:
                         this.flush(State.SCALAR);
                         this.current_key = line.data;
@@ -120,6 +119,85 @@ public:
             default: assert(false);
         }
     }//Parser.parse
+    unittest {
+        Parser p;
+        void reset() { p = new Parser(); };
+        run_test({//Scalar state accumulates data
+            reset();
+            p.parse("$scalar\n");
+            assert(p.state == State.SCALAR);
+            assert(p.current_key == "scalar");
+            p.parse(":hello\n");
+            p.parse(":goodbye\n");
+            assert(p.current_key == "scalar");
+            assert(p.scalar_data == "hello\ngoodbye");
+        });
+        run_test({//Scalar state transitions correctly
+            reset();
+            p.parse("$scalar\n");
+            p.parse("$new scalar\n");
+            assert(p.state == State.SCALAR);
+            assert(p.current_key == "new scalar");
+            p.parse("@new array\n");
+            assert(p.state == State.ARRAY);
+            assert(p.current_key == "new array");
+        });
+        run_test({//Array state accumulates data
+            reset();
+            p.parse("@array\n");
+            assert(p.state == State.ARRAY);
+            assert(p.current_key == "array");
+            p.parse(":part1\n");
+            p.parse(":part2");
+            assert(p.current_key == "array");
+            assert(p.scalar_data == "part1\npart2");
+        });
+        run_test({//Array state accumulates elements
+            reset();
+            p.parse("@array\n");
+            p.parse(":part1\n");
+            p.parse(":part2\n");
+            p.parse(",elem2\n");
+            p.parse(",");
+            assert(p.current_key == "array");
+            assert(p.array_data == ["part1\npart2", "elem2"]);
+        });
+        run_test({//Array state transitions correctly
+            reset();
+            p.parse("@array\n");
+            p.parse("@new array\n");
+            assert(p.state == State.ARRAY);
+            assert(p.current_key == "new array");
+            p.parse("$new scalar\n");
+            assert(p.state == State.SCALAR);
+            assert(p.current_key == "new scalar");
+        });
+        run_test({//All states reject invalid input.
+            reset();
+            assert(does_throw!SyntaxError(p.parse("gobbledygook")));
+            assert(does_throw!SyntaxError(p.parse(":data\n")));
+            assert(does_throw!SyntaxError(p.parse(",elem\n")));
+            p.parse("$scalar\n");
+            assert(does_throw!SyntaxError(p.parse("gobbledygook")));
+            assert(does_throw!SyntaxError(p.parse(",elem")));
+            p.parse("@array\n");
+            assert(does_throw!SyntaxError(p.parse("gobbledygook")));
+        });
+        run_test({//Blank lines are not a problem
+            reset();
+            p.parse("$scalar\n");
+            p.parse(":\n");
+            p.parse(":goodbye\n");
+            p.parse(":");
+            assert(p.scalar_data == "\ngoodbye\n");
+        });
+        run_test({//Empty data is not a problem
+            reset();
+            p.parse("$scalar\n");
+            p.parse(":");
+            assert(p.scalar_data == "");
+        });
+    }
     //TESTME
 
     void flush(State new_state=State.END)
@@ -134,29 +212,33 @@ public:
     }
     body {
         if (this.state == State.SCALAR) {
-            this.outer.scalars[current_key] = this.scalar_data;
+            if (this.outer !is null)
+                this.outer.scalars[current_key] = this.scalar_data;
             this.scalar_data = "";
         }
         else {
             assert(this.state == State.ARRAY);
             this.buffer_elem();
-            this.outer.vectors[current_key] = this.array_data;
+            if (this.outer !is null)
+                this.outer.vectors[current_key] = this.array_data;
             this.array_data = [];
         }
         this._current_key = null;
         this.state = new_state;
     }
-    //TESTME
+    //no unittests, contracts are sufficient until integration tests
 
 private:
     void buffer_data(string data) {
         if (this.scalar_data != "") this.scalar_data ~= '\n';
         this.scalar_data ~= data;
     }
+    //TESTME
     void buffer_elem() {
         this.array_data ~= this.scalar_data;
         this.scalar_data = "";
     }
+    //TESTME
 
     static final
     struct Line {
